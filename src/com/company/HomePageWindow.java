73a49732ed6f6a684cb74ee00eabb0c9acb0dec4
Maria Wiik <wiikcandido.maria@syk.fi>
2023-08-16 08:20:12 +0300

    CS IA going

--- src/com/company/HomePageWindow.java
+++ src/com/company/HomePageWindow.java
@@ -9,8 +9,12 @@ public class HomePageWindow extends JFrame implements ActionListener
 {
     private JMenuBar menuBar;
     private JPanel p;
+    private JPanel pCenter;
     private JButton toSelectPlayWindow, toAddDeleteWindow, toPhotosWindow, toPlaylistsWindow,
-    toQueuePreviewWindow, toSongsWindow;
+            toQueuePreviewWindow, toSongsWindow;
+    private JLabel searchT, selectYourMoodT;
+    private JMenuItem menuSelect, menuAddDelete, menuPhotos, menuPlaylists, menuSongs, menuQueue, menuHome;
+    // nameOfPlaylistT, nameOfSongT -> these two change according to what is playingggg
 
 
     public HomePageWindow()
@@ -19,63 +23,66 @@ public class HomePageWindow extends JFrame implements ActionListener
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLayout(new FlowLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        /*
-        p = new JPanel();
-        p.setLayout(null);
 
-        toSelectPlayWindow = new JButton("Select song, playlist, or queue");
-        toSelectPlayWindow.setFocusable(false);
-        toSelectPlayWindow.setBounds(0, 30, 195, 30);
-        p.add(toSelectPlayWindow);
+        menuBar = new JMenuBar();
+        menuSelect = new JMenuItem("Select What to Play Page");
+        menuAddDelete = new JMenuItem("Add / Delete Page");
+        menuPhotos = new JMenuItem("Photos Page");
+        menuPlaylists = new JMenuItem("Playlists Page");
+        menuSongs = new JMenuItem("Songs Page");
+        menuQueue = new JMenuItem("Queue Page");
+        menuHome = new JMenuItem("Home Page");
+        // add the action listeners to lead to the other windows
+        menuBar.add(menuAddDelete); menuBar.add(menuPhotos); menuBar.add(menuQueue);
+        menuBar.add(menuSongs); menuBar.add(menuSelect); menuBar.add(menuPlaylists);
+        menuBar.add(menuHome);
+        setJMenuBar(menuBar);
 
-         */
+        // adding a panel so I can add buttons here, buttons weren't moving without this :/
+        // i think the layout is too small and not positioned correctly
+        // i need to move it so my buttons show up
+        pCenter = new JPanel();
+        pCenter.setLayout(null);
+        pCenter.setPreferredSize(new Dimension(1260, 650));
+        // pCenter.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(pCenter, BorderLayout.CENTER);
 
-        /*
-        toAddDeleteWindow = new JButton("Add/Delete");
-        toAddDeleteWindow.setFocusable(false);
-        //toAddDeleteWindow.setBounds(30, 0, 54, 30);
-        toPhotosWindow = new JButton("Photos");
-        toPhotosWindow.setFocusable(false);
+        // get the positions right
+        toSelectPlayWindow = new JButton("Select a Song or Playlist");
+        toSelectPlayWindow.setBounds(550, 50, 225, 35);
+        pCenter.add(toSelectPlayWindow);
 
-        toPlaylistsWindow = new JButton("Playlists");
-        toPlaylistsWindow.setFocusable(false);
-
-        toQueuePreviewWindow = new JButton("Queue Preview");
-        toQueuePreviewWindow.setFocusable(false);
+        toAddDeleteWindow = new JButton("Add / Delete");
+        toAddDeleteWindow.setBounds(30, 90, 100, 35);
+        pCenter.add(toAddDeleteWindow);
 
         toSongsWindow = new JButton("Songs");
-        toSongsWindow.setFocusable(false);
+        toSongsWindow.setBounds(30, 400, 100, 35);
+        pCenter.add(toSongsWindow);
 
-         */
+        toPlaylistsWindow = new JButton("Playlists");
+        toPlaylistsWindow.setBounds(30, 500, 100, 35);
+        pCenter.add(toPlaylistsWindow);
 
-        // toSelectPlayWindow.addActionListener(this);
-        /*
-        toAddDeleteWindow.addActionListener(this::actionPerformed2);
-        toSongsWindow.addActionListener(this::actionPerformed3);
-        toPlaylistsWindow.addActionListener(this::actionPerformed4);
-        toPhotosWindow.addActionListener(this::actionPerformed5);
-        toQueuePreviewWindow.addActionListener(this::actionPerformed6);
-        addButtons();
+        toQueuePreviewWindow = new JButton("Queue");
+        toQueuePreviewWindow.setBounds(1100, 400, 100, 35);
+        pCenter.add(toQueuePreviewWindow);
 
-         */
+        toPhotosWindow = new JButton("Photos");
+        toPhotosWindow.setBounds(1100, 90, 100, 35);
+        pCenter.add(toPhotosWindow);
 
-        // add(p);
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
-        JButton jbutton = new JButton("Click");
-        jbutton.setBounds(10, 10, 75, 25);  // x, y, width, height
-        panel.add(jbutton);
-        add(panel);
+        searchT = new JLabel("Search");
+        searchT.setBounds(30, 150, 100, 35);
+        pCenter.add(searchT);
+
+        selectYourMoodT = new JLabel("Select Your Mood");
+        selectYourMoodT.setBounds(1100, 500, 150, 35);
+        pCenter.add(selectYourMoodT);
+
+        // is the search thingy where the user writes a "text field"?
+
         setVisible(true);
-    }
-    public void addButtons()
-    {
-        p.add(toSelectPlayWindow, BorderLayout.PAGE_START);
-        p.add(toAddDeleteWindow, BorderLayout.LINE_START);
-        p.add(toPhotosWindow, BorderLayout.LINE_END);
-        p.add(toPlaylistsWindow, BorderLayout.PAGE_END);
-        p.add(toSongsWindow, BorderLayout.LINE_START);
-        p.add(toQueuePreviewWindow, BorderLayout.LINE_END);
     }
 
     @Override
@@ -86,30 +93,35 @@ public class HomePageWindow extends JFrame implements ActionListener
         SelectPlayWindow.setVisible(true); // display SelectPlayWindow
         dispose(); // close home page
     }
+
     public void actionPerformed2(ActionEvent e)
     {
         JFrame AddDeleteWindow = new AddDeleteWindow(); // open another JFrame
         AddDeleteWindow.setVisible(true); // display SelectPlayWindow
         dispose(); // close home page
     }
+
     public void actionPerformed3(ActionEvent e)
     {
         JFrame SongsWindow = new SongsWindow(); // open another JFrame
         SongsWindow.setVisible(true); // display SelectPlayWindow
         dispose(); // close home page
     }
+
     public void actionPerformed4(ActionEvent e)
     {
         JFrame PlaylistsWindow = new PlaylistsWindow(); // open another JFrame
         PlaylistsWindow.setVisible(true); // display SelectPlayWindow
         dispose(); // close home page
     }
+
     public void actionPerformed5(ActionEvent e)
     {
         JFrame PhotosWindow = new PhotosWindow(); // open another JFrame
         PhotosWindow.setVisible(true); // display SelectPlayWindow
         dispose(); // close home page
     }
+
     public void actionPerformed6(ActionEvent e)
     {
         JFrame QueuePreviewWindow = new QueuePreviewWindow(); // open another JFrame
