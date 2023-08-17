96f5d9371d7d7aa693d0a622e814b5f12db54320
Maria Wiik <wiikcandido.maria@syk.fi>
2023-08-17 20:09:38 +0300

    Most, if not all home page buttons added, must be positioned + add actionListeners

--- src/com/company/HomePageWindow.java
+++ src/com/company/HomePageWindow.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class HomePageWindow extends JFrame implements ActionListener
 {
@@ -12,35 +16,63 @@ public class HomePageWindow extends JFrame implements ActionListener
     private JPanel pCenter;
     private JButton toSelectPlayWindow, toAddDeleteWindow, toPhotosWindow, toPlaylistsWindow,
             toQueuePreviewWindow, toSongsWindow;
+    private JButton sadFaceButton, chillFaceButton, happyFaceButton, veryHappyButton;
     private JLabel searchT, selectYourMoodT;
-    private JMenuItem menuSelect, menuAddDelete, menuPhotos, menuPlaylists, menuSongs, menuQueue, menuHome;
+    private JMenuItem menuSelect, menuAddDelete, menuPhotos, menuPlaylists, menuSongs, menuQueue;
+    private ImageIcon sadIcon, chillIcon, happyIcon, veryHappyIcon;
+    private JButton filterButton, loopButton, nextSongButton, pauseButton, playButton, previousSongButton,
+    randomButton, tenBackButton, tenForwardButton;
+    // Do I need a search button? Or will the search happen when the user presses enter
+    private ImageIcon filterIcon, loopIcon, nextSongIcon, pauseIcon, playIcon, previousSongIcon,
+            randomIcon, tenBackIcon, tenForwardIcon;
+    private JTextField searchBox;
+
     // nameOfPlaylistT, nameOfSongT -> these two change according to what is playingggg
 
-
-    public HomePageWindow()
+    public HomePageWindow() throws IOException
     {
         super("Audio Player");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLayout(new FlowLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        //<editor-fold desc="Menu Bar">
         menuBar = new JMenuBar();
-        menuSelect = new JMenuItem("Select What to Play Page");
-        menuAddDelete = new JMenuItem("Add / Delete Page");
-        menuPhotos = new JMenuItem("Photos Page");
-        menuPlaylists = new JMenuItem("Playlists Page");
-        menuSongs = new JMenuItem("Songs Page");
-        menuQueue = new JMenuItem("Queue Page");
-        menuHome = new JMenuItem("Home Page");
-        // add the action listeners to lead to the other windows
-        menuBar.add(menuAddDelete); menuBar.add(menuPhotos); menuBar.add(menuQueue);
-        menuBar.add(menuSongs); menuBar.add(menuSelect); menuBar.add(menuPlaylists);
-        menuBar.add(menuHome);
+        JMenu fileMenu = new JMenu("Home Page");
+
+        menuSelect = new JMenuItem("Select What to Play");
+        menuSelect.addActionListener(this::actionPerformed);
+
+        menuAddDelete = new JMenuItem("Add / Delete");
+        menuAddDelete.addActionListener(this::actionPerformed2);
+
+        menuPhotos = new JMenuItem("Photos");
+        menuPhotos.addActionListener(this::actionPerformed5);
+
+        menuPlaylists = new JMenuItem("Playlists");
+        menuPlaylists.addActionListener(this::actionPerformed4);
+
+        menuSongs = new JMenuItem("Songs");
+        menuSongs.addActionListener(this::actionPerformed3);
+
+        menuQueue = new JMenuItem("Queue");
+        menuQueue.addActionListener(this::actionPerformed6);
+
+        fileMenu.add(menuAddDelete);
+        fileMenu.add(menuPhotos);
+        fileMenu.add(menuQueue);
+        fileMenu.add(menuSongs);
+        fileMenu.add(menuSelect);
+        fileMenu.add(menuPlaylists);
+        menuBar.add(fileMenu);
         setJMenuBar(menuBar);
+        //</editor-fold>
 
         // adding a panel so I can add buttons here, buttons weren't moving without this :/
         // i think the layout is too small and not positioned correctly
         // i need to move it so my buttons show up
+
+        //<editor-fold desc="Main Buttons">
         pCenter = new JPanel();
         pCenter.setLayout(null);
         pCenter.setPreferredSize(new Dimension(1260, 650));
@@ -50,37 +82,128 @@ public class HomePageWindow extends JFrame implements ActionListener
         // get the positions right
         toSelectPlayWindow = new JButton("Select a Song or Playlist");
         toSelectPlayWindow.setBounds(550, 50, 225, 35);
+        toSelectPlayWindow.addActionListener(this::actionPerformed);
         pCenter.add(toSelectPlayWindow);
 
         toAddDeleteWindow = new JButton("Add / Delete");
-        toAddDeleteWindow.setBounds(30, 90, 100, 35);
+        toAddDeleteWindow.setBounds(30, 90, 150, 35);
+        toAddDeleteWindow.addActionListener(this::actionPerformed2);
         pCenter.add(toAddDeleteWindow);
 
         toSongsWindow = new JButton("Songs");
         toSongsWindow.setBounds(30, 400, 100, 35);
+        toSongsWindow.addActionListener(this::actionPerformed3);
         pCenter.add(toSongsWindow);
 
         toPlaylistsWindow = new JButton("Playlists");
         toPlaylistsWindow.setBounds(30, 500, 100, 35);
+        toPlaylistsWindow.addActionListener(this::actionPerformed4);
         pCenter.add(toPlaylistsWindow);
 
         toQueuePreviewWindow = new JButton("Queue");
-        toQueuePreviewWindow.setBounds(1100, 400, 100, 35);
+        toQueuePreviewWindow.setBounds(1100, 150, 100, 35);
+        toQueuePreviewWindow.addActionListener(this::actionPerformed6);
         pCenter.add(toQueuePreviewWindow);
 
         toPhotosWindow = new JButton("Photos");
         toPhotosWindow.setBounds(1100, 90, 100, 35);
+        toPhotosWindow.addActionListener(this::actionPerformed5);
         pCenter.add(toPhotosWindow);
+        //</editor-fold>
 
         searchT = new JLabel("Search");
         searchT.setBounds(30, 150, 100, 35);
         pCenter.add(searchT);
 
+        filterIcon = new ImageIcon("src/middleSectionHP/filterIcon.png");
+        filterButton = new JButton(filterIcon);
+        filterButton.setBounds(110, 200, 50, 30);
+        pCenter.add(filterButton);
+
+        searchBox = new JTextField();
+        searchBox.setBounds(50, 200, 100, 30);
+        pCenter.add(searchBox);
+
+        BufferedImage searchIcon = ImageIO.read(new File("src/middleSectionHP/searchIcon.png"));
+        JLabel searchIconLabel = new JLabel(new ImageIcon(searchIcon));
+        searchIconLabel.setBounds(30, 200, 30, 30);
+        pCenter.add(searchIconLabel);
+
+        //<editor-fold desc="Mood Stuff">
         selectYourMoodT = new JLabel("Select Your Mood");
-        selectYourMoodT.setBounds(1100, 500, 150, 35);
+        selectYourMoodT.setBounds(950, 400, 150, 35);
         pCenter.add(selectYourMoodT);
 
+        sadIcon = new ImageIcon("src/moodIcons/sadIcon.png");
+        sadFaceButton = new JButton(sadIcon);
+        sadFaceButton.setBounds(800, 520, 100, 90);
+        pCenter.add(sadFaceButton);
+
+        chillIcon = new ImageIcon("src/moodIcons/chillIcon.png");
+        chillFaceButton = new JButton(chillIcon);
+        chillFaceButton.setBounds(1100, 520, 100, 90);
+        pCenter.add(chillFaceButton);
+
+        happyIcon = new ImageIcon("src/moodIcons/happyIcon.png");
+        happyFaceButton = new JButton(happyIcon);
+        happyFaceButton.setBounds(900, 520, 100, 90);
+        pCenter.add(happyFaceButton);
+
+        veryHappyIcon = new ImageIcon("src/moodIcons/veryHappyIcon.png");
+        veryHappyButton = new JButton(veryHappyIcon);
+        veryHappyButton.setBounds(1000, 520, 100, 90);
+        pCenter.add(veryHappyButton);
+        //</editor-fold>
+
+        // middle section -> pause play stuff
+        // create a play button -> change to this when pause is clicked, and vice versa
+        // adjust the coordinates cause this looks weird
+
+        //<editor-fold desc="Middle Stuff">
+        pauseIcon = new ImageIcon("src/middleSectionHP/pauseIcon.jpg");
+        pauseButton = new JButton(pauseIcon);
+        pauseButton.setBounds(600, 300, 90, 70);
+        pCenter.add(pauseButton);
+
+        // i have to somehow leave this hidden until the pause button is clicked
+        playIcon = new ImageIcon("src/middleSectionHP/playIcon.jpg");
+        playButton = new JButton(playIcon);
+        playButton.setBounds(600, 300, 90, 70);
+        // pCenter.add(playButton);
+
+        previousSongIcon = new ImageIcon("src/middleSectionHP/previousSongIcon.jpg");
+        previousSongButton = new JButton(previousSongIcon);
+        previousSongButton.setBounds(400, 300, 75, 60);
+        pCenter.add(previousSongButton);
+
+        nextSongIcon = new ImageIcon("src/middleSectionHP/nextSongIcon.jpg");
+        nextSongButton = new JButton(nextSongIcon);
+        nextSongButton.setBounds(800, 300, 75, 60);
+        pCenter.add(nextSongButton);
+
+        loopIcon = new ImageIcon("src/middleSectionHP/loopIcon.jpg");
+        loopButton = new JButton(loopIcon);
+        loopButton.setBounds(460, 400, 80, 70);
+        pCenter.add(loopButton);
+
+        randomIcon = new ImageIcon("src/middleSectionHP/randomIcon.jpg");
+        randomButton = new JButton(randomIcon);
+        randomButton.setBounds(560, 400, 80, 70);
+        pCenter.add(randomButton);
+
+        tenBackIcon = new ImageIcon("src/middleSectionHP/tenBackIcon.jpg");
+        tenBackButton = new JButton(tenBackIcon);
+        tenBackButton.setBounds(310, 400, 50, 30);
+        pCenter.add(tenBackButton);
+
+        tenForwardIcon = new ImageIcon("src/middleSectionHP/tenForwardIcon.jpg");
+        tenForwardButton = new JButton(tenForwardIcon);
+        tenForwardButton.setBounds(700, 400, 50, 30);
+        pCenter.add(tenForwardButton);
+        //</editor-fold>
+
         // is the search thingy where the user writes a "text field"?
+        // progress bar for songggg
 
         setVisible(true);
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new HomePageWindow());
+        SwingUtilities.invokeLater(() ->
+        {
+            try
+            {
+                new HomePageWindow();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        });
     }
 }
